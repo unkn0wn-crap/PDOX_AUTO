b9f38b233ff108d3ac4e7d1c88d5804062ca9cea
ghost <error.2418362@gmail.com>
2024-10-30 05:04:27 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -17,9 +17,9 @@ API_ID = int(environ.get('API_ID', 6435225)) #API ID
 API_HASH = environ.get('API_HASH', '4e984ea35f854762dcde906dce426c2d') #API HASH
 BOT_TOKEN = environ.get('BOT_TOKEN', '5527818445:AAE7TLprBfyUuQvYZsaOesQ0F-9C2sl2I80') #BOT TOKEN
 DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://arnavgupta0078:arnav@cluster3301.ojyvd.mongodb.net/retryWrites=true&w=majority') #MONGO DB
-OWNER_ID = "6908541951" #OWNER ID
-MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', "-1002386259040"))#YOUR MAIN CHANNEL ID
-ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', "-1002386259040"))#YOUR ARCHIVE CHANNEL
-MESSAGE_ID = int(environ.get('MESSAGE_ID', "2")) #SUB CHANNEL STATUS ID
+OWNER_ID = "6259443940" #OWNER ID
+MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', "-1002231792362"))#YOUR MAIN CHANNEL ID
+ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', "-1002231792362"))#YOUR ARCHIVE CHANNEL
+MESSAGE_ID = int(environ.get('MESSAGE_ID', "42")) #SUB CHANNEL STATUS ID
 
 soheru = Client('SoheruBots', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
